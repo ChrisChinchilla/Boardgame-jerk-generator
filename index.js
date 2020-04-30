b1303a76233060dbfdca544101454a1a4343c8d9
Chris Chinchilla <chris@chrischinchilla.com>
2020-04-30 12:37:19 +0200

    Add mechanic

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 exports.generateLexicon = function() {
   let tracery = require("tracery-grammar");
 
+  let mechanic = require("./mechanic.json");
   let you_are = require("./you_are.json");
   let doing = require("./doing.json");
   let with_what = require("./with_what.json");
   let in_place = require("./in.json");
 
   const grammar = tracery.createGrammar({
+    mechanic: mechanic,
     you_are: you_are,
     doing: doing,
     with_what: with_what,
     in: in_place,
-    origin: ["You are #you_are# #doing# #with_what# in #in#"]
+    origin: ["A #mechanic# game, where you are #you_are# #doing# #with_what# in #in#"]
   });
 
   grammar.addModifiers(tracery.baseEngModifiers);
